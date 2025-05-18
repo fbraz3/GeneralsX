TheSuperHackers/GeneralsGameCode

[GEN][ZH] Fix undefined behavior with MemoryPoolObject::deleteInstance (#870)

Generals/Code/GameEngine/Include/Common/GameMemory.h
@@ -756,13 +756,13 @@ protected:
 	
 public: 
 
-	void deleteInstance() 
+	static void deleteInstance(MemoryPoolObject* object) 
 	{	
-		if (this)
+		if (object)
 		{
-			MemoryPool *pool = this->getObjectMemoryPool(); // save this, since the dtor will nuke our vtbl
-			this->~MemoryPoolObject();	// it's virtual, so the right one will be called.
-			pool->freeBlock((void *)this); 
+			MemoryPool *pool = object->getObjectMemoryPool(); // save this, since the dtor will nuke our vtbl
+			object->~MemoryPoolObject();	// it's virtual, so the right one will be called.
+			pool->freeBlock((void *)object); 
 		}
 	} 
 };
@@ -906,7 +906,7 @@ public:
 	MemoryPoolObjectHolder(MemoryPoolObject *mpo = NULL) : m_mpo(mpo) { }
 	void hold(MemoryPoolObject *mpo) { DEBUG_ASSERTCRASH(!m_mpo, ("already holding")); m_mpo = mpo; }
 	void release() { m_mpo = NULL; }
-	~MemoryPoolObjectHolder() { m_mpo->deleteInstance(); }
+	~MemoryPoolObjectHolder() { MemoryPoolObject::deleteInstance(m_mpo); }
 };
 
 
@@ -919,7 +919,11 @@ public:
 	you really want by including this macro 
 */
 #define MEMORY_POOL_DELETEINSTANCE_VISIBILITY(ARGVIS)\
-ARGVIS:	void deleteInstance() { MemoryPoolObject::deleteInstance(); } public: 
+ARGVIS: \
+	static void deleteInstance(MemoryPoolObject* object) { \
+		MemoryPoolObject::deleteInstance(object); \
+	} \
+public: 
 
 
 #define EMPTY_DTOR(CLASS) inline CLASS::~CLASS() { }

Generals/Code/GameEngine/Include/Common/GameMemoryNull.h
@@ -108,9 +108,9 @@ protected:
 
 public:
 
-	void deleteInstance() 
+	static void deleteInstance(MemoryPoolObject* object) 
 	{
-		delete this;
+		delete object;
 	}
 };
 

fbraz3/GeneralsX

Generals/Code/GameEngine/Include/Common/GameMemory.h
@@ -756,13 +756,13 @@ protected:
 	
 public: 
 
-	void deleteInstance() 
+	static void deleteInstance(MemoryPoolObject* mpo) 
 	{	
-		if (this)
+		if (mpo)
 		{
-			MemoryPool *pool = this->getObjectMemoryPool(); // save this, since the dtor will nuke our vtbl
-			this->~MemoryPoolObject();	// it's virtual, so the right one will be called.
-			pool->freeBlock((void *)this); 
+			MemoryPool *pool = mpo->getObjectMemoryPool(); // save this, since the dtor will nuke our vtbl
+			mpo->~MemoryPoolObject();	// it's virtual, so the right one will be called.
+			pool->freeBlock((void *)mpo); 
 		}
 	} 
 };
@@ -906,7 +906,7 @@ public:
 	MemoryPoolObjectHolder(MemoryPoolObject *mpo = NULL) : m_mpo(mpo) { }
 	void hold(MemoryPoolObject *mpo) { DEBUG_ASSERTCRASH(!m_mpo, ("already holding")); m_mpo = mpo; }
 	void release() { m_mpo = NULL; }
-	~MemoryPoolObjectHolder() { m_mpo->deleteInstance(); }
+	~MemoryPoolObjectHolder() { MemoryPoolObject::deleteInstance(m_mpo); }
 };
 
 
@@ -919,7 +919,11 @@ public:
 	you really want by including this macro 
 */
 #define MEMORY_POOL_DELETEINSTANCE_VISIBILITY(ARGVIS)\
-ARGVIS:	void deleteInstance() { MemoryPoolObject::deleteInstance(); } public: 
+ARGVIS: \
+	static void deleteInstance(MemoryPoolObject* object) { \
+		MemoryPoolObject::deleteInstance(object); \
+	} \
+public: 
 
 
 #define EMPTY_DTOR(CLASS) inline CLASS::~CLASS() { }

Generals/Code/GameEngine/Include/Common/GameMemoryNull.h
@@ -108,9 +108,9 @@ protected:
 
 public:
 
-	void deleteInstance() 
+	static void deleteInstance(MemoryPoolObject* mpo) 
 	{
-		delete this;
+		delete mpo;
 	}
 };
 