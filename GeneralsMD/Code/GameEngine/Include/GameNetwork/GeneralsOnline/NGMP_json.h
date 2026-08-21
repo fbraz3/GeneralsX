8e9717898c2021fcef9b16c1678c2bee176ac93f
Felipe Keller Braz <fbraz3@gmail.com>
2026-08-21 20:29:14 -0300

    fix(build): protect nlohmann json from snprintf macro pollution on MSVC

--- GeneralsMD/Code/GameEngine/Include/GameNetwork/GeneralsOnline/NGMP_json.h
+++ GeneralsMD/Code/GameEngine/Include/GameNetwork/GeneralsOnline/NGMP_json.h
@@ -3,8 +3,16 @@
 
 #pragma push_macro("min")
 #pragma push_macro("max")
+#pragma push_macro("snprintf")
+#pragma push_macro("vsnprintf")
+#pragma push_macro("_snprintf")
+#pragma push_macro("_vsnprintf")
 #undef min
 #undef max
+#undef snprintf
+#undef vsnprintf
+#undef _snprintf
+#undef _vsnprintf
 
 #if __has_include(<nlohmann/json.hpp>)
 #include <nlohmann/json.hpp>
@@ -16,6 +24,10 @@
 #include "GameNetwork/GeneralsOnline/json.hpp"
 #endif
 
+#pragma pop_macro("_vsnprintf")
+#pragma pop_macro("_snprintf")
+#pragma pop_macro("vsnprintf")
+#pragma pop_macro("snprintf")
 #pragma pop_macro("max")
 #pragma pop_macro("min")
 
